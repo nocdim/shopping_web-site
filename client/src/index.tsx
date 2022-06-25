56cefb29e87666f38575d103a45f18a66ec4cbd6
nocdim <nokkudimu@gmail.com>
2022-06-25 09:41:16 +0300

    clean project

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
+// store - объект, который содержит несколько методов (получить - getState, изменить state - dispatch и...
+// подписаться на изменения в состоянии)
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
