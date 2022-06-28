f25e065c487c7248a65eb8d7747cab6c92754b9f
nocdim <nokkudimu@gmail.com>
2022-06-28 13:38:22 +0300

    tutorial done without mutations

--- client/src/components/ProductContainer.tsx
+++ client/src/components/ProductContainer.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { productAPI } from '../services/ProductService'
 import ProductItem from './ProductItem'
 
 const ProductContainer = () => {
-    const {data: products} = productAPI.useFetchAllProductsQuery(8)
+    const [productsLimit, setProductsLimit] = useState(8)
+    const { data: products, error, isLoading } = productAPI.useFetchAllProductsQuery(productsLimit)
     return (
-        <div>{products && products.map(product =>
-                <ProductItem 
-                key={product.id}
-                product={product} 
+        <div>
+            {isLoading && <h1>Loading...</h1>}
+            {error && <h1>Loading error...</h1>}
+            <div>{products && products.map(product =>
+                <ProductItem
+                    key={product.id}
+                    product={product}
                 />
             )}</div>
+        </div>
     )
 }
 
