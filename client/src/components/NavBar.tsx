b734f572b4fc5e1912832b8393fd5a93f0d23aec
nocdim <nokkudimu@gmail.com>
2022-06-28 20:54:24 +0300

    project frozen for now (i need to learn typescript first)

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './styled/NavBar'
+
+const NavBar = () => {
+    return (
+        <>
+            <Container>
+                <button></button>
+            </Container>
+        </>
+    )
+}
+
+export default NavBar
