a911ce339ec074a0241baf5407d03ed2b75f0eb0
nocdim <nokkudimu@gmail.com>
2022-06-29 16:01:36 +0300

    about section done

---
+++ client/src/components/styled/About.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    margin: 0;
+    padding: 0;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: calc(100vh - 8rem);
+    span {
+        font-size: 30px;
+        font-weight: 500;
+        padding: 1rem 3rem;
+        a {
+            color: #ff3838;
+            text-decoration: none;
+        }
+    }
+`
+const Footer = styled.div`
+    display: flex;
+    margin: 0;
+    padding: 0;
+    align-items: center;
+    justify-content: center;
+
+`
+
+export {
+    Container,
+    Footer
+}
--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container, Footer } from '../components/styled/About'
+import { SHOP_ROUTE } from '../utils/consts'
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    return (
+        <>
+        <Container>
+            <span>Even though this company was founded in 2022 and haven't got many followers, we still strive to be better and better by day</span>
+            <span>You can purchase our products <a href={SHOP_ROUTE}>here</a> and make us even greater than before</span>
+        </Container>
+        <Footer>Project was made by Dima Nochyovkin</Footer>
+        </>
+        
+    )
 }
 
 export default About
