961d1624ab76acc3a5d95d94347566ac3c886465
nocdim <nokkudimu@gmail.com>
2022-07-02 00:23:25 +0300

    small fix

--- client/src/components/styled/Shop.tsx
+++ client/src/components/styled/Shop.tsx
@@ -4,13 +4,12 @@ const StyledTypeContainer = styled.div`
     display: flex;
     justify-content: space-between;
     position: absolute;
-    min-height: calc(100vh - 1.5rem);
+    min-height: inherit;
     top: 125px;
     left: 75px;
     @media (max-width: 1000px) {
         top: 250px;
         left: 50px;
-        min-height: inherit;
     }
 `
 
@@ -18,13 +17,12 @@ const StyledBrandContainer = styled.div`
     display: flex;
     justify-content: space-between;
     position: absolute;
-    min-height: calc(100vh - 1.5rem);
+    min-height: inherit;
     top: 125px;
     right: 75px;
     @media (max-width: 1000px) {
         top: 250px;
         right: 50px;
-        min-height: inherit;
     }
 `
 
