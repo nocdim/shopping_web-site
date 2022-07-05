da3ebdde69d5cac1e64e1b6adb9b63189215c1b1
nocdim <nokkudimu@gmail.com>
2022-07-05 11:32:38 +0300

    changing

--- client/src/components/styled/Main.tsx
+++ client/src/components/styled/Main.tsx
@@ -1,16 +1,43 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    font-size: 56px;
     font-weight: 500;
     display: flex;
     margin: 0;
     padding: 0;
     align-items: center;
     justify-content: center;
-    height: calc(100vh);
+    height: 100vh;
+    flex-direction: column;
+
+    span {
+        font-size: 56px;
+    }
+`
+
+const Slider = styled.div`
+    height: 360px;
+    width: 840px;
+    margin: 2rem 0rem 0rem 0rem;
+    border: 1px solid black;
+`
+const ButtonContainer = styled.div`
+    height: inherit;
+    width: inherit;
+    display: flex;
+    justify-content: space-between;
+    button {
+        height: inherit;
+        width: 100px;
+        border: none;
+        outline: none;
+        font-size: 50px;
+        background-color: transparent;
+    }
 `
 
 export {
-    Container
+    Container,
+    Slider,
+    ButtonContainer
 }
