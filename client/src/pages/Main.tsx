a3d1dca5a48ccbc434fed111b80dbedb6722167d
nocdim <nokkudimu@gmail.com>
2022-07-05 11:31:12 +0300

    slider on main in progress

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Container } from '../components/styled/Main'
+import { ButtonContainer, Container, Slider } from '../components/styled/Main'
 
 const Main = () => {
   return (
     <Container>
-        Welcome to Nokkushop
+        <span>Welcome to Nokkushop</span>
+        <Slider>
+          <ButtonContainer>
+            <button></button>
+            <button></button>
+          </ButtonContainer>
+        </Slider>
     </Container>
   )
 }
